075bde63bd25842e44889f31d4e6e16599d71626
Srinivasssaravanan <137177741+Srinivasssaravanan@users.noreply.github.com>
2026-01-04 15:00:27 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import recipeRoutes from './routes/recipeRoutes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
 
 app.use(express.json());
 app.use('/api/recipes', recipeRoutes);
